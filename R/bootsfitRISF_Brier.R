44c6c3d1064bb955dad996895e01f528cdbbad13
Shanpeng Li <shanpengli@Shanpengs-Mini.askey.com>
2022-09-30 22:50:39 -0700

    update codes

---
+++ R/bootsfitRISF_Brier.R
@@ -0,0 +1,108 @@
+##' @export
+##'
+
+bootsfitRISF_Brier <- function(i, seed, N, increment, beta, tau, gamma1,
+                             alpha1, vee1, lambda1, CL,
+                             CU, covbw, quadpoint, maxiter, 
+                             n.cv, landmark.time, horizon.time, method = "GH") {
+  
+  data <- simJMdataRISF(seed = seed + i, N = N, increment = increment,
+                      beta = beta, tau = tau, gamma1 = gamma1, 
+                      alpha1 = alpha1, vee1 = vee1, 
+                      lambda1 = lambda1, 
+                      CL = CL, CU = CU, covbw = covbw)
+  ydata <- data$ydata
+  cdata <- data$cdata
+  
+  folds <- caret::groupKFold(c(1:nrow(cdata)), k = n.cv)
+  Brier.cv <- list()
+  
+  for (t in 1:n.cv) {
+    train.ydata <- ydata[ydata$ID %in% folds[[t]], ]
+    train.cdata <- cdata[cdata$ID %in% folds[[t]], ]
+    
+    fit <- try(JMMLSM(cdata = train.cdata, ydata = train.ydata, 
+                      long.formula = Y ~ Z1 + Z2 + Z3 + time,
+                      surv.formula = Surv(survtime, cmprsk) ~ var1 + var2 + var3,
+                      variance.formula = ~ Z1 + Z2 + Z3 + time, 
+                      quadpoint = quadpoint, random = ~ 1|ID), silent = TRUE)
+    
+    if ('try-error' %in% class(fit)) {
+      Brier.cv[[t]] <- NULL
+    } else if (fit$iter == maxiter) {
+      Brier.cv[[t]] <- NULL
+    } else {
+      
+      val.ydata <- ydata[!ydata$ID %in% folds[[t]], ]
+      val.cdata <- cdata[!cdata$ID %in% folds[[t]], ]
+      
+      ## fit a Kalplan-Meier estimator
+      fitKM <- survfit(Surv(survtime, cmprsk == 0) ~ 1, data = val.cdata)
+      
+      val.cdata <- val.cdata[val.cdata$survtime > landmark.time, ]
+      val.ydata <- val.ydata[val.ydata$ID %in% val.cdata$ID, ]
+      val.ydata <- val.ydata[val.ydata$time <= landmark.time/increment, ]
+      
+      survfit <- try(survfitJMMLSM(fit, seed = seed, ynewdata = val.ydata, cnewdata = val.cdata, 
+                                   u = horizon.time, method = method, 
+                                   Last.time = rep(landmark.time, nrow(val.cdata)),
+                                   obs.time = "time", quadpoint = quadpoint), silent = TRUE)
+      
+      if ('try-error' %in% class(survfit)) {
+        Brier.cv[[t]] <- NULL
+      } else {
+        Surv <- as.data.frame(matrix(0, nrow = nrow(val.cdata), ncol = 2))
+        colnames(Surv) <- c("ID", "Surv")
+        Surv$ID <- val.cdata$ID
+        Gs <- summary(fitKM, times = landmark.time)$surv
+        mean.Brier <- matrix(NA, nrow = length(horizon.time), ncol = 1)
+        mean.MAE <- matrix(NA, nrow = length(horizon.time), ncol = 1)
+        for (j in 1:length(horizon.time)) {
+          fitKM.horizon <- try(summary(fitKM, times = horizon.time[j]), silent = TRUE)
+          if ('try-error' %in% class(fitKM.horizon)) {
+            mean.Brier[j, 1] <- NA
+          } else {
+            Gu <- fitKM.horizon$surv
+            ## true counting process
+            N1 <- vector()
+            Gt <- vector()
+            W.IPCW <- vector()
+            for (i in 1:nrow(Surv)) {
+              if (val.cdata[i, surv.var[1]] <= horizon.time[j] && val.cdata[i, surv.var[2]] == 1) {
+                N1[i] <- 1
+                Gt[i] <- summary(fitKM, times = val.cdata[i, surv.var[1]])$surv
+              } else {
+                N1[i] <- 0
+                Gt[i] <- NA
+              }
+              
+              if (val.cdata[i, surv.var[1]] > horizon.time[j]) {
+                W.IPCW[i] <- 1/(Gu/Gs)
+              } else if (val.cdata[i, surv.var[1]] <= horizon.time[j] && val.cdata[i, surv.var[2]] == 1) {
+                W.IPCW[i] <- 1/(Gt[i]/Gs)
+              } else {
+                W.IPCW[i] <- NA
+              }
+            }
+            ## extract estimated Survival probability
+            for (k in 1:nrow(Surv)) {
+              Surv[k, 2] <- survfit$Pred[[k]][j, 2]
+            }
+            
+            RAWData.Brier <- data.frame(Surv, N1, W.IPCW)
+            colnames(RAWData.Brier)[1:2] <- c("ID", "Surv")
+            RAWData.Brier$Brier <- RAWData.Brier$W.IPCW*
+              abs(1 - RAWData.Brier$Surv - RAWData.Brier$N1)^2
+            mean.Brier[j, 1] <- sum(RAWData.Brier$Brier, na.rm = TRUE)/nrow(RAWData.Brier)
+          }
+        }
+        Brier.cv[[t]] <- mean.Brier
+      }
+      
+    }
+    
+  }
+  
+  return(Brier.cv)
+  
+}
---
+++ R/simfitRISF_DPBrier.R
@@ -0,0 +1,33 @@
+##' @export
+##'
+
+simfitRISF_DPBrier <- function(sim = 10, N = 500, seed = 100, increment = 0.7, beta = c(5, 1.5, 2, 1, 2),
+                             tau = c(0.5, 0.5, -0.2, 0.2, 0.05),
+                             gamma1 = c(1, 0.5, 0.5),
+                             alpha1 = 1,
+                             vee1 = 0.5,
+                             lambda1 = 0.05,
+                             CL = 4,
+                             CU = 8,
+                             covbw = matrix(c(0.5, 0.25, 0.25, 0.5), nrow = 2, ncol = 2),
+                             maxiter = 1000, quadpoint = 10, 
+                             landmark.time = 2, horizon.time = seq(3, 6, by = 1), n.cv = 3,
+                             method = "GH", ncores = 10) {
+  
+  
+  ParaMatrixRaw <- parallel::mclapply(1:sim, bootsfitRISF_Brier,
+                                      seed = seed, N = N, increment = increment, 
+                                      beta = beta, tau = tau, 
+                                      gamma1 = gamma1, 
+                                      alpha1 = alpha1, 
+                                      vee1 = vee1, 
+                                      lambda1 = lambda1, 
+                                      CL = CL, CU = CU, covbw = covbw, 
+                                      quadpoint = quadpoint, maxiter = maxiter, 
+                                      n.cv = n.cv, landmark.time = landmark.time, 
+                                      horizon.time = horizon.time, method = method, 
+                                      mc.cores = ncores)
+  
+  return(ParaMatrixRaw)
+  
+}
